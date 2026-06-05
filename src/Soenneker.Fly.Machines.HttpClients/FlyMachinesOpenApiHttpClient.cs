dffec13b88a6a07bbe608d82a177f4ba7b5b9c71
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:11 -0500

    xml commenting

--- src/Soenneker.Fly.Machines.HttpClients/FlyMachinesOpenApiHttpClient.cs
+++ src/Soenneker.Fly.Machines.HttpClients/FlyMachinesOpenApiHttpClient.cs
@@ -45,11 +45,18 @@ public sealed class FlyMachinesOpenApiHttpClient : IFlyMachinesOpenApiHttpClient
         }, cancellationToken);
     }
 
+    /// <summary>
+    /// Releases resources used by the current instance.
+    /// </summary>
     public void Dispose()
     {
         _httpClientCache.RemoveSync(nameof(FlyMachinesOpenApiHttpClient));
     }
 
+    /// <summary>
+    /// Asynchronously releases resources used by the current instance.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public ValueTask DisposeAsync()
     {
         return _httpClientCache.Remove(nameof(FlyMachinesOpenApiHttpClient));
